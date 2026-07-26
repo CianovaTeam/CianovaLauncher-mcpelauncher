259a65df3128e63f7d2e7e33817220b0ebb1cef9
PlaGaPlusDev <plagaplusdev@outlook.com>
2026-07-26 15:30:53 -0400

    AppImage workflow + portable fixes

--- assets/CianovaLauncher.sh
+++ assets/CianovaLauncher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+SELF_DIR="$(dirname "$(readlink -f "$0")")"
 
-#Exportar LIBS y BINS
-export PATH="/bin:$PATH"
-export MCPELAUNCHER_DATA_DIR="/libs_mc"
+export PATH="${SELF_DIR}/bin:$PATH"
+export LD_LIBRARY_PATH="${SELF_DIR}/_internal:${LD_LIBRARY_PATH}"
+export MCPELAUNCHER_DATA_DIR="${SELF_DIR}/lib"
+export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox --ignore-gpu-blocklist"
+export QT_QUICK_BACKEND="software"
 
-exec ./CianovaLauncherMCPE "$@"
+exec "${SELF_DIR}/CianovaLauncherMCPE" "$@"
